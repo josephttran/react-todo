b65d27171dea5b48d52f87246cd247db527b40ef
JosephTTran <42635012+JosephTTran@users.noreply.github.com>
2018-09-23 21:30:39 -0700

    css

--- src/components/list/Task.js
+++ src/components/list/Task.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import './Task.css'
 
 class Task extends Component {
   render() {
     return (
       <div className="task">
-        <span style={{ textDecoration: this.props.todo.done ? 'line-through' : 'none' }}>
+        <span className="task-name" style={{ textDecoration: this.props.todo.done ? 'line-through' : 'none' }}>
           {this.props.todo.value}
         </span> 
-        <button onClick={() => this.props.handleClick(this.props.index)}>
+        <button className="task-btn" onClick={() => this.props.handleClick(this.props.index)}>
           {this.props.todo.done ? 'Undo' : 'Complete'}
         </button> 
       </div>
