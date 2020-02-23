52f82e747f2aa4c36afc36f9fcc8f83b041503d5
josephttran <42635012+josephttran@users.noreply.github.com>
2020-02-22 20:16:03 -0800

    Style header

---
+++ src/assets/menuIcon.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const HamburgerMenu = () => 
+<svg viewBox="0 0 100 60" width="16" height="16">
+  <rect width="100" height="20"></rect>
+  <rect y="30" width="100" height="20"></rect>
+  <rect y="60" width="100" height="20"></rect>
+</svg>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { HamburgerMenu } from '../assets/menuIcon';
+
 function Header() {
   return (
-    <header className="bg-blue-200">
-      <div>
+    <header className="bg-blue-700 flex justify-between p-4">
+      <div className="flex items-center visible sm:invisible">
+        <HamburgerMenu />
+      </div>
+      <div className="text-white text-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
         Todo List App
       </div>
+      <div></div>
     </header>
   )
 }
